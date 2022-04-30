1868c9f1e933c411ad2275de8dabda6f192ddd47
limjaehee <rhak39@gmail.com>
2022-04-30 17:48:37 +0900

    퍼즐게임 수정

--- puzzle/index.js
+++ puzzle/index.js
@@ -1,18 +1,19 @@
 const YLEN = 3, // y축
     XLEN = 3, // x축
-    BLANK = 0,
+    BLANK = 0, //빈칸 값
     boardEl = document.querySelector("#puzzleTable"),
-    buttonEl = document.querySelector(".button");
+    buttonEl = document.querySelector(".button"),
+    clearMessage = document.querySelector(".clear-message");
 
-let board = [],
-    blank = YLEN * XLEN - 1;
+let board = [], //보드 배열
+    blank = YLEN * XLEN - 1; //빈칸위치
 
 //퍼즐 Array 생성
 function initPuzzle() {
-    board = [];
-    blank = YLEN * XLEN - 1;
     for (let y = 0; y < YLEN; y++) {
         board[y] = [];
+
+        console.log(board[y]);
         for (let x = 0; x < XLEN; x++) {
             let idx = y * XLEN + x;
             if (idx < blank) board[y][x] = idx + 1;
@@ -41,8 +42,7 @@ function updateBoard() {
     for (let y = 0; y < YLEN; y++) {
         html += "<tr>";
         for (let x = 0; x < XLEN; x++) {
-            if (board[y][x] !== BLANK)
-                html += '<td class="puzzle-item">' + board[y][x] + "</td>";
+            if (board[y][x] !== BLANK) html += '<td class="puzzle-item">' + board[y][x] + "</td>";
             else html += '<td class="puzzle-item blank">&nbsp;</td>';
         }
 
@@ -53,8 +53,7 @@ function updateBoard() {
 }
 
 function moveTo(to) {
-    board[parseInt(blank / YLEN, 10)][blank % XLEN] =
-        board[parseInt(to / YLEN, 10)][to % XLEN];
+    board[parseInt(blank / YLEN, 10)][blank % XLEN] = board[parseInt(to / YLEN, 10)][to % XLEN];
     board[parseInt(to / YLEN, 10)][to % XLEN] = BLANK;
     blank = to;
     updateBoard();
@@ -75,30 +74,62 @@ function move(d, human) {
             if (parseInt(blank / YLEN, 10) < YLEN - 1) moveTo(blank + XLEN);
             break;
     }
+
+    if (human) isEnd();
 }
 
 initPuzzle();
 
 //새로 시작하기 버튼
-buttonEl.addEventListener("click", initPuzzle);
+buttonEl.addEventListener("click", () => {
+    reset();
+    initPuzzle();
+});
 
 window.onkeydown = (e) => {
-    switch (e.which) {
-        case 37:
-            move("l", true);
-            break;
-        case 38:
-            move("u", true);
-            break;
-        case 39:
-            move("r", true);
-            break;
-        case 40:
-            move("d", true);
-            break;
+    if (!clearMessage.innerText) {
+        switch (e.which) {
+            case 37:
+                move("l", true);
+                break;
+            case 38:
+                move("u", true);
+                break;
+            case 39:
+                move("r", true);
+                break;
+            case 40:
+                move("d", true);
+                break;
+        }
     }
 };
 
-function keyDownEvt(e) {
-    console.log(e);
+//게임 종료
+function isEnd() {
+    for (let y = 0; y < YLEN; y++) {
+        for (let x = 0; x < XLEN; x++) {
+            const expect = y * XLEN + x + 1;
+            if (y * XLEN + x < YLEN * XLEN - 1) {
+                if (board[y][x] !== expect) {
+                    return;
+                }
+            }
+        }
+    }
+    clear("Clear✨");
+}
+
+//성공 메시지
+function clear(text) {
+    clearMessage.innerText = text;
+    clearMessage.style.display = "block";
+}
+
+//다시 시작하기
+function reset() {
+    board = []; //보드 배열 리셋
+    blank = YLEN * XLEN - 1; //빈칸 위치 리셋
+    clearMessage.innerText = null;
+    clearMessage.style.display = "none";
 }
